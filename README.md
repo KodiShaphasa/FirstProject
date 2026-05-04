b43d92e0a910f8e8c4ef3b1ac140050248d7d720
KodiShaphasa <kodishaphasa12@gmail.com>
2026-05-04 18:43:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+projekti
+pershkrimi i projektit
+teknologjite e projektit
+hapat per te hapur projektin
+qellimin etj
---
+++ js/js.js
@@ -0,0 +1,6 @@
+function formContact(event){
+    event.preventDefault();
+    let FullName = document.getElementById("ImputFullName").value;
+    let Email = document.getElementById("exampleInputEmail1").value;
+    let Comment = document.getElementById("Comment").value;
+}
